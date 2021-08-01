8f159592469126740174b95d148b17353d07fc98
scava <sorzesimone1@gmail.com>
2021-08-01 13:27:09 +0200

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
 	root: true,
-	extends: ['@react-native-community', 'prettier'],
+	extends: [
+		'@react-native-community',
+		'prettier',
+		'plugin:cypress/recommended',
+	],
 	plugins: ['prettier'],
 	rules: {
 		'prettier/prettier': 'error',
+		'import/prefer-default-export': 'off',
 		'react/jsx-filename-extension': [
 			2,
 			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
