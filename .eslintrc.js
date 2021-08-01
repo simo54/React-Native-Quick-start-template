d5432a3895ea072701fac982e0273cfaa322613c
scava <sorzesimone1@gmail.com>
2021-08-01 12:41:38 +0200

    fixing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
 	root: true,
-	extends: '@react-native-community',
+	extends: ['@react-native-community', 'prettier'],
 	plugins: ['prettier'],
 	rules: {
 		'prettier/prettier': 'error',
+		'react/jsx-filename-extension': [
+			2,
+			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+		],
 	},
 };
